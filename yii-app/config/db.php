bd766fb713920d93cdc4d9b52989d21ae94d2f81
Kevin Fu <kevinfu_1999@hotmail.com>
2017-07-04 20:43:27 +0800

    数据库相关
    Signed-off-by: Kevin Fu <kevinfu_1999@hotmail.com>

--- yii-app/config/db.php
+++ yii-app/config/db.php
@@ -3,7 +3,11 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'yii2basic',
+    'password' => 'R785vaCU7Q5yxje8',
     'charset' => 'utf8',
+    'on afterOpen' => function($event) {
+        // $event->sender refers to the DB connection
+        $event->sender->createCommand("SET time_zone = '+8:00'")->execute();
+    }
 ];
