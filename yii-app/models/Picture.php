4f30dc5bb955346ac90c601aef36a5bb783a5393
Kevin Fu <kevinfu_1999@hotmail.com>
2017-07-06 11:44:00 +0800

    我发现了GridView可以直接使用array中的数据 好吧我蠢

--- yii-app/models/Picture.php
+++ yii-app/models/Picture.php
@@ -15,33 +15,12 @@ class Picture extends Model
 {
     protected static $rootDir = '/pictures';
 
-    public $filename;
-    public $filesize;
-    public $modifyTime;
-    public $sourceUrl;
+    public $pageSize;
+    public $listOver;
+    public $recordSet;
 
-    protected $recordSet;
-    protected $currentIndex = 0; //Current Index in Recordset
     protected $context = ''; //As is stated in Tencent COS
-    protected $listover; //Bool
-    protected $pageSize;
-
     protected $api;
-    protected $_hasData;
-
-    public function attributeLabels()
-    {
-        return [
-            'filename' => '文件名',
-            'filesize' => '文件大小',
-            'modifyTime' => '修改时间',
-            'sourceUrl' => '文件地址'
-        ];
-    }
-
-    public function hasData(){
-        return $this->_hasData;
-    }
 
     function __construct(array $config = [])
     {
@@ -49,45 +28,13 @@ class Picture extends Model
         parent::__construct($config);
     }
 
-    protected function assignProperties($info){
-        $this->filename = $info['name'];
-        $this->filesize = $info['filesize'];
-        $this->modifyTime = $info['mtime'];
-        $this->sourceUrl = $info['source_url'];
-        $this->_hasData = true;
+    public function listFiles(){
+        if(defined('YII_DEBUG') and YII_DEBUG) assert(isset($pageSize));
+        $info = $this->api->listFolder($this->api->bucketName(),self::$rootDir,$this->pageSize,'eListFileOnly',0,$this->context);
+        if($info['code']) throw new \Exception($info['message']);
+        $info=$info['data'];
+        $this->context = $info['context'];
+        $this->listOver = $info['listover'];
+        $this->recordSet = $info['infos'];
     }
-
-    protected function analyzeRecordSet($recordSet){
-        if($recordSet['code']) throw new \Exception($recordSet['message']);
-        $recordSet = $recordSet['data'];
-        $this->context = $recordSet['context'];
-        $this->listover = $recordSet['listover'];
-        $this->recordSet = $recordSet['infos'];
-    }
-
-    public function openList($pageSize){
-        if(isset($this->recordSet)) return;
-        $this->pageSize = $pageSize;
-        $this->analyzeRecordSet($this->api->listFolder($this->api->bucketName(),self::$rootDir,$this->pageSize,'eListFileOnly'));
-    }
-
-    public function next(){
-        if(!isset($this->recordSet[$this->currentIndex])){
-            $this->currentIndex = 0;
-            if($this->listover){
-                $this->_hasData = false;
-                return;
-            }else
-                $this->analyzeRecordSet($this->api->listFolder($this->api->bucketName(),self::$rootDir,$this->pageSize,'eListFileOnly',0,$this->context));
-        }
-        $this->assignProperties($this->recordSet[$this->currentIndex]);
-        $this->currentIndex++;
-    }
-
-    public function closeList(){
-        unset($this->recordSet);
-        $this->currentIndex = 0;
-        $this->context = '';
-    }
-    
 }
---
+++ yii-app/views/picture/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin Fu 1999
+ * Date: 2017-07-05
+ * Time: 12:47
+ */
+$this->title = '图片文件列表';
+?>
+
+<div class="picture-index">
+
+</div>
